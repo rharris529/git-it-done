436a47f43b76ae93e1d112d32953d187c6861868
rharris529 <jordandine@gmail.com>
2022-01-28 02:53:04 -0600

    initial code

---
+++ assets/js/single.js
@@ -0,0 +1,57 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
+var getReposIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos" + repo + "/issues?direction=asc";
+
+    fetch(apiUrl).then(function(response){
+        // request was succesful
+        if (response.ok) {
+            response.json().then(function(data){
+                // pass response data to dom function
+                displayIssues(data);
+            });
+        } else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
+
+var displayIssues = function() {
+
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
+    issueContainerEl.appendChild(issueEl);
+
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on GitHub
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create a span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issues is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issues)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+    }
+};
+
+getReposIssues("rharris529/git-it-done");
